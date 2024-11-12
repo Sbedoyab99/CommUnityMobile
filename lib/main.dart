37c02123c8b584e5047daaa92653551afb5be8d2
Santiago Bedoya <santiagobedoya1128808@correo.itm.edu.co>
2024-11-12 17:30:21 -0500

    Creating home screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mi App Flutter',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
       initialRoute: '/',
       routes: {
         '/': (context) => const SplashScreen(),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:community/models/News.dart';
 import 'package:community/models/User.dart';
+import 'package:community/services/news_service.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:community/services/auth_service.dart';
@@ -11,11 +13,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
+  bool isLoading = true;
   String? token;
   User? user;
+  List<News> news = [];
 
   final AuthService _authService = AuthService();
+  final NewsService _newsService = NewsService();
 
   @override
   void initState() {
@@ -24,8 +28,15 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> loadAsync() async {
+    setState(() {
+      isLoading = true;
+    });
     token = await loadToken();
-    loadUser(token!);
+    user = await loadUser(token!);
+    news = await loadNews(token!, user!.residentialUnitId!);
+    setState(() {
+      isLoading = false;
+    });
   }
 
   Future<String?> loadToken() async {
@@ -35,20 +46,139 @@ class _HomePageState extends State<HomePage> {
     if (token != null) {
       return token;
     } else {
-      print("No se encontró el token");
       return null;
     }
   }
 
-  Future<void> loadUser(String token) async {
+  Future<User?> loadUser(String token) async {
     final user = await _authService.getUser(token);
+    return user;
+  }
+
+  Future<List<News>> loadNews(String token, num id) async {
+    final news = await _newsService.fetchNews(token, id);
+    return news;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: const Center(child: Text('Bienvenido')),
+      resizeToAvoidBottomInset: false,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(56),
+        child: AppBar(
+          backgroundColor: Colors.deepPurple,
+          flexibleSpace: Padding(
+            padding: const EdgeInsets.only(
+              top: 40.0,
+              left: 16.0,
+              right: 16.0,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  isLoading ? 'Cargando...' : user!.residentialUnit!.name!,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.person),
+                  color: Colors.white,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RefreshIndicator(
+                  onRefresh: loadAsync,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: const ClampingScrollPhysics(),
+                    itemCount: news.length,
+                    itemBuilder: (context, index) {
+                      final newsItem = news[index];
+                      return Card(
+                        margin: const EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            newsItem.picture != null
+                                ? Image.network(
+                                    newsItem.picture!,
+                                    fit: BoxFit.contain,
+                                  )
+                                : Image.asset(
+                                    'assets/defaultImage.avif',
+                                    fit: BoxFit.cover,
+                                  ),
+                            Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Text(
+                                newsItem.title!,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8),
+                              child: Text(
+                                newsItem.content!.characters
+                                    .take(100)
+                                    .toString(),
+                                style: const TextStyle(fontSize: 14),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+      bottomNavigationBar: SafeArea(
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people),
+              label: 'Visitas',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.mail),
+              label: 'Correspondencia',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.note_add),
+              label: 'PQRS',
+            ),
+          ],
+          onTap: (index) {
+            if (index == 2) {
+              Navigator.pushNamed(context, '/settings');
+            }
+          },
+        ),
+      ),
     );
   }
 }
