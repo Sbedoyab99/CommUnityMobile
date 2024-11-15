628d1e01fd21cabb79d3cbd014d8f240284aa388
Santiago Bedoya <santiagobedoya1128808@correo.itm.edu.co>
2024-11-15 18:47:45 -0500

    Creating modal to read news

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:community/models/News.dart';
 import 'package:community/models/User.dart';
+import 'package:community/screens/news_screen.dart';
 import 'package:community/services/news_service.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -60,6 +61,26 @@ class _HomePageState extends State<HomePage> {
     return news;
   }
 
+  void viewNews(BuildContext context, News news) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(news.title!),
+          content: NewsScreen(news: news),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return isLoading
@@ -75,38 +96,46 @@ class _HomePageState extends State<HomePage> {
                   itemCount: news.length,
                   itemBuilder: (context, index) {
                     final newsItem = news[index];
-                    return Card(
-                      margin: const EdgeInsets.all(10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          newsItem.picture != null
-                              ? Image.network(
-                                  newsItem.picture!,
-                                  fit: BoxFit.contain,
-                                )
-                              : Image.asset(
-                                  'assets/defaultImage.avif',
-                                  fit: BoxFit.cover,
+                    return InkWell(
+                      onTap: () {
+                        viewNews(context, newsItem);
+                      },
+                      child: Card(
+                        margin: const EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            newsItem.picture != null
+                                ? Image.network(
+                                    newsItem.picture!,
+                                    fit: BoxFit.contain,
+                                  )
+                                : Image.asset(
+                                    'assets/defaultImage.avif',
+                                    fit: BoxFit.cover,
+                                  ),
+                            Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Text(
+                                newsItem.title!,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                          Padding(
-                            padding: const EdgeInsets.all(8),
-                            child: Text(
-                              newsItem.title!,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8),
-                            child: Text(
-                              newsItem.content!.characters.take(100).toString(),
-                              style: const TextStyle(fontSize: 14),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8),
+                              child: Text(
+                                newsItem.content!.characters
+                                    .take(100)
+                                    .toString(),
+                                style: const TextStyle(fontSize: 14),
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     );
                   },
